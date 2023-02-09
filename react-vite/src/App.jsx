d4d842deff0489b940b892e9bfd498ebf519007d
Panda <thebose.vicky@gmail.com>
2023-02-09 11:12:58 +0530

    v2

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import { useEffect } from "react";
+
+const API_URL = 'http://www.omdbapi.com?apikey=2d99223';
 
 const App = () => {
-  return (
-    <div className="App">
-    <h1>VITE REACT SEEMS Good</h1>
-    </div>
-  )
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+      console.log(data.Search);
+    }
+
+    useEffect(() => {
+       searchMovies('Spiderman');
+    }, []);
+
+    return (
+        <h1>App</h1>
+    );
 }
 
-export default App
+export default App;
